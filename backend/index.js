a3ed3832c8d13574c7c1835725418391aaf3a89c
Andrew Aghoghovwia <androyt1@gmail.com>
2023-11-02 10:18:26 +0000

    updates

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,9 @@ mongoose
 
 const app = express()
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+app.use(
+    cors({ origin: 'https://androy-mern-blog.netlify.app/', credentials: true })
+)
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
